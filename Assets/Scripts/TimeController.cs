33891fbce8edbdf077f6fdcf011598fb059efa56
harun <harun.dogan@stud.fh-campuswien.ac.at>
2025-05-10 14:54:25 +0200

    air & game over

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeController : MonoBehaviour
+{
+    public Image timerImage;
+    public float maxTime = 5.0f;
+    float timeRemaining;
+    public GameObject gameOver;
+    public GameObject tryAgainButton;
+    public GameObject player;
+    
+
+    void Start() {
+        timeRemaining = maxTime;
+    }
+
+    void Update() {
+        if (timeRemaining > 0) {
+            timeRemaining -= Time.deltaTime;
+            timerImage.fillAmount = timeRemaining / maxTime;
+        } else {
+            gameOver.SetActive(true);
+            tryAgainButton.SetActive(true);
+            player.SetActive(false);
+        }
+    }
+
+    public void tryAgain() {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+}
