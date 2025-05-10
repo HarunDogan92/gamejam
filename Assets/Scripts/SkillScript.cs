17ae5db0a71b529a27ebaa9c642471e177bd9547
harun <harun.dogan@stud.fh-campuswien.ac.at>
2025-05-10 12:36:11 +0200

    skilltree

---
+++ Assets/Scripts/SkillScript.cs
@@ -0,0 +1,54 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillScript : MonoBehaviour
+{
+    public Button button;
+    public RawImage imagesToShow;
+    public Sprite unlockedSprite;
+    public TextMeshProUGUI unlockText;
+    public TextMeshProUGUI unlockPayText;
+    public String afterUnlockText;
+    public String afterUnlockPayText;
+
+    public bool isFinalButton = false;
+    public Button finalButton;
+    public bool isFinalButtonActive;
+
+
+    void Start()
+    {
+        button.onClick.AddListener(() => onClick());
+    }
+
+    void onClick()
+    {
+        imagesToShow.gameObject.SetActive(true);
+
+
+        Image btnImage = button.GetComponent<Image>();
+        if (btnImage != null && unlockedSprite != null)
+        {
+            btnImage.sprite = unlockedSprite;
+        }
+        if(finalButton){
+            finalButton.gameObject.SetActive(true);
+        }
+
+        //unlockText.gameObject.SetActive(false);
+        unlockText.text = afterUnlockText;
+        unlockPayText.text = afterUnlockPayText;
+        unlockPayText.color = Color.green;
+        RectTransform rect = unlockPayText.GetComponent<RectTransform>();
+        Vector2 pos = rect.anchoredPosition;
+        pos.y = -60f;
+        rect.anchoredPosition = pos;
+        
+        if(finalButton){
+            finalButton.gameObject.SetActive(true);
+            isFinalButtonActive = true;
+        }
+    }
+}
